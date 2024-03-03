8416a1777d5e92408844e9561d50fa5fe67dd506
Von P. Walden <v.walden@wsu.edu>
2024-03-03 04:10:59 +0000

    Improved the readability of this Panel script

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,7 @@ pn.extension(template='fast')
 #####################################################################
 # ....Determine today's and yesterday's dates
 today = pd.Timestamp.now('UTC')
-yesterday = pd.Timestamp.now('UTC')-pd.Timedelta('25h')
+yesterday = pd.Timestamp.now('UTC')-pd.Timedelta('24h')
 
 #####################################################################
 # ....Read data from SLEIGH and MVP
@@ -21,47 +21,93 @@ power = xr.concat([power_yesterday, power_today], dim='time')
 # ....Select the past 24 hours only
 power = power.sel(time=slice(yesterday.to_datetime64(), today.to_datetime64()))
 
+BatterySOC = xr.DataArray(power.BatterySOC.values, coords=[("time", power.time.values)], name='SOC [%]')
+BatteryWatts = xr.DataArray(power.BatteryWatts.values, coords=[("time", power.time.values)], name='BattS [W]')
+
+SolarWatts_Tot = xr.DataArray(power.SolarWatts_East.values + power.SolarWatts_South.values + power.SolarWatts_West.values, coords=[("time", power.time.values)], name='Solar [W]')
+WindWatts = xr.DataArray(power.WindWatts.values, coords=[("time", power.time.values)], name='Wind [W]')
+
+ACOutputWatts = xr.DataArray(power.ACOutputWatts.values, coords=[("time", power.time.values)], name='AC [W]')
+DCInverterWatts = xr.DataArray(power.DCInverterWatts.values, coords=[("time", power.time.values)], name='DCInverterWatts')
+DCWatts = SolarWatts_Tot + WindWatts - BatteryWatts - DCInverterWatts
+DCWatts.name = 'DC [W]'
+
 # %%
 #################### MINIMUM VIABLE POWERSUPPLY #####################
 #####################################################################
 # ....Row of battery and power numbers and gauges
-p1 = pn.indicators.Number(name='Battery SOC', value=power.BatterySOC.resample(time='1h').mean().values[-1], format='{value:.0f}%', colors=[(25, 'red'), (50, 'gold'), (100, 'green')])
+p1 = pn.indicators.Number(name='Battery SOC', 
+                          value=power.BatterySOC.resample(time='1h').mean().values[-1], 
+                          format='{value:.0f}%', 
+                          colors=[(25, 'red'), (50, 'gold'), (100, 'green')]
+)
 
-p2 = pn.indicators.Gauge(name="DC Inv Power", value=np.round(power.DCInverterWatts.resample(time='1h').mean().values[-1]), bounds=(0, 250), format='{value} W', colors=[(0.4, 'green'), (0.6, 'gold'), (1, 'red')],custom_opts={"pointer": {"itemStyle": {"color": 'black'}}})
-#p2 = pn.indicators.Number(name='DC Power', value=power.DCWatts.resample(time='1h').mean().values[-1], format='{value:.0f}W', colors=[(100, 'green'), (200, 'yellow'), (300, 'red')])
+p2 = pn.indicators.Gauge(name="DC Inv Power", 
+                         value=np.round(power.DCInverterWatts.resample(time='1h').mean().values[-1]), 
+                         bounds=(0, 250), 
+                         format='{value} W', 
+                         colors=[(0.4, 'green'), (0.6, 'gold'), (1, 'red')],
+                         custom_opts={"pointer": {"itemStyle": {"color": 'black'}}}
+)
 
-p3 = pn.indicators.Gauge(name="AC Power", value=np.round(power.ACOutputWatts.resample(time='1h').mean().values[-1]), bounds=(0, 1500), format='{value} W',colors=[(0.167, 'green'), (0.3, 'gold'), (1, 'red')],custom_opts={"pointer": {"itemStyle": {"color": 'black'}}})
-#p3 = pn.indicators.Number(name='AC Power', value=power.ACOutputWatts.resample(time='1h').mean().values[-1], format='{value:.0f}W', colors=[(250, 'green'), (350, 'yellow'), (500, 'red')])
+p3 = pn.indicators.Gauge(name="AC Power", 
+                         value=np.round(power.ACOutputWatts.resample(time='1h').mean().values[-1]), 
+                         bounds=(0, 1500), 
+                         format='{value} W',
+                         colors=[(0.167, 'green'), (0.3, 'gold'), (1, 'red')],
+                         custom_opts={"pointer": {"itemStyle": {"color": 'black'}}}
+)
 
-p4 = pn.indicators.Number(name='Battery Voltage', value=power.BatteryVoltage.resample(time='1h').mean().values[-1], format='{value:.0f} V', colors=[(40, 'red'), (45, 'yellow'), (55, 'green'), (60, 'yellow'), (70, 'red') ])
+p4 = pn.indicators.Number(name='Battery Voltage', 
+                          value=power.BatteryVoltage.resample(time='1h').mean().values[-1], 
+                          format='{value:.0f} V', 
+                          colors=[(40, 'red'), (45, 'yellow'), (55, 'green'), (60, 'yellow'), (70, 'red')]
+)
 
-row = pn.Row(p1, pn.Spacer(sizing_mode='stretch_width'), p2, pn.Spacer(sizing_mode='stretch_width'), p3, pn.Spacer(sizing_mode='stretch_width'), p4)
+row = pn.Row(p1, pn.Spacer(sizing_mode='stretch_width'), 
+             p2, pn.Spacer(sizing_mode='stretch_width'), 
+             p3, pn.Spacer(sizing_mode='stretch_width'), 
+             p4
+)
 
 #####################################################################
 # ....Column of time-series plots of battery and power variables
-BatterySOC = xr.DataArray(power.BatterySOC.values, coords=[("time", power.time.values)], name='SOC [%]')
-BatteryWatts = xr.DataArray(power.BatteryWatts.values, coords=[("time", power.time.values)], name='BattS [W]')
-
 #overlay = hv.Curve([1, 2, 3], vdims=['A']) * hv.Curve([2, 3, 4], vdims=['A']) * hv.Curve([3, 2, 1], vdims=['B'])
 #overlay.opts(multi_y=True)
 
-p5 = BatterySOC.hvplot(grid=True, line_width=5, height=400, title='BATTERIES', color='lightseagreen', vdims=['SOC [%]'], responsive=True).opts(active_tools=['box_zoom']) * BatteryWatts.hvplot(grid=True, line_width=5, height=400, color='lightblue', vdims=['Batts [W]'], responsive=True)
+p5 = BatterySOC.hvplot(grid=True, 
+                       line_width=5, 
+                       height=400, 
+                       title='BATTERIES (most recent measurement from ' + pd.to_datetime(power.time.data[-1]).strftime('%H:%M') +')', 
+                       color='lightseagreen', responsive=True).opts(active_tools=['box_zoom']) * 
+    BatteryWatts.hvplot(grid=True, 
+                        line_width=5, 
+                        height=400, 
+                        color='lightblue', 
+                        responsive=True)
 p5.opts(multi_y=True)
 
-SolarWatts_Tot = xr.DataArray(power.SolarWatts_East.values + power.SolarWatts_South.values + power.SolarWatts_West.values, coords=[("time", power.time.values)], name='Solar [W]')
-
-WindWatts = xr.DataArray(power.WindWatts.values, coords=[("time", power.time.values)], name='Wind [W]')
-
-p6 = SolarWatts_Tot.hvplot(grid=True, line_width=5, height=400, title='RENEWABLES', color='sienna', vdim=['Solar [W]'], responsive=True).opts(active_tools=['box_zoom']) * WindWatts.hvplot(line_width=5, height=400, color='darkkhaki', vdims=['Wind [W]'], responsive=True)
+p6 = SolarWatts_Tot.hvplot(grid=True, 
+                           line_width=5, 
+                           height=400, 
+                           title='RENEWABLES (most recent measurement from '+pd.to_datetime(power.time.data[-1]).strftime('%H:%M')+')', color='sienna', responsive=True).opts(active_tools=['box_zoom']) * 
+        WindWatts.hvplot(line_width=5, 
+                         height=400, 
+                         color='darkkhaki', 
+                         responsive=True)
 p6.opts(multi_y=True)
 
-ACOutputWatts = xr.DataArray(power.ACOutputWatts.values, coords=[("time", power.time.values)], name='AC [W]')
-DCInverterWatts = xr.DataArray(power.DCInverterWatts.values, coords=[("time", power.time.values)], name='DCInverterWatts')
-#DCWatts = SolarWatts_Tot + WindWatts - BatteryWatts - DCInverterWatts
-DCWatts = SolarWatts_Tot - BatteryWatts - DCInverterWatts
-DCWatts.name = 'DC [W]'
 
-p7 = ACOutputWatts.hvplot(grid=True, line_width=5, height=400, title='OUTPUT', color='firebrick', vdims=['AC [W]'], responsive=True).opts(active_tools=['box_zoom']) * DCWatts.hvplot(line_width=5, height=400, color='blue', vdims=['DC [W]'], responsive=True)
+p7 = ACOutputWatts.hvplot(grid=True, 
+                          line_width=5, 
+                          height=400, 
+                          title='OUTPUT (most recent measurement from '+pd.to_datetime(power.time.data[-1]).strftime('%H:%M')+')', 
+                          color='firebrick', 
+                          responsive=True).opts(active_tools=['box_zoom']) * 
+            DCWatts.hvplot(line_width=5, 
+                           height=400, 
+                           color='blue', 
+                           responsive=True)
 p7.opts(multi_y=True)
 
 col = pn.Column(p5, p6, p7)

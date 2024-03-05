054fe8d07f893e5989cb33199f7892a8281aad53
mg <michael.r.gallagher@noaa.gov>
2024-03-05 05:07:49 +0000

    create main loop and abstract tabs, run server in main()

--- dashboard.py
+++ dashboard.py
@@ -1,130 +1,259 @@
-# %%
+#!/usr/bin/env python3
 import numpy as np
 import pandas as pd
 import xarray as xr
 import hvplot.xarray # noqa
 import holoviews as hv
 import panel as pn
-pn.extension('echarts')
-pn.extension(template='fast')
 
-#####################################################################
-# ....Determine today's and yesterday's dates
-today = pd.Timestamp.now('UTC')
-yesterday = pd.Timestamp.now('UTC')-pd.Timedelta('24h')
+import time
 
-#####################################################################
-# ....Read data from SLEIGH and MVP
-power_yesterday = xr.open_dataset('/data/power/level2/power.mvp.level2.1min.' + yesterday.strftime('%Y%m%d') + '.000000.nc')
-power_today = xr.open_dataset('/data/power/level2/power.mvp.level2.1min.' + today.strftime('%Y%m%d') + '.000000.nc')
-power = xr.concat([power_yesterday, power_today], dim='time')
-# ....Select the past 24 hours only
-power = power.sel(time=slice(yesterday.to_datetime64(), today.to_datetime64()))
+#pn.extension(design='material', template='material')
 
-BatterySOC = xr.DataArray(power.BatterySOC.values, coords=[("time", power.time.values)], name='SOC [%]')
-BatteryWatts = xr.DataArray(power.BatteryWatts.values, coords=[("time", power.time.values)], name='BattS [W]')
+def process_data():
 
-SolarWatts_Tot = xr.DataArray(power.SolarWatts_East.values + power.SolarWatts_South.values + power.SolarWatts_West.values, coords=[("time", power.time.values)], name='Solar [W]')
-WindWatts = xr.DataArray(power.WindWatts.values, coords=[("time", power.time.values)], name='Wind [W]')
+    pn.extension('echarts', template='fast', nthreads=4, notifications=False)
 
-ACOutputWatts = xr.DataArray(power.ACOutputWatts.values, coords=[("time", power.time.values)], name='AC [W]')
-DCInverterWatts = xr.DataArray(power.DCInverterWatts.values, coords=[("time", power.time.values)], name='DCInverterWatts')
-DCWatts = SolarWatts_Tot + WindWatts - BatteryWatts - DCInverterWatts
-DCWatts.name = 'DC [W]'
 
-# %%
-#################### MINIMUM VIABLE POWERSUPPLY #####################
-#####################################################################
-# ....Row of battery and power numbers and gauges
-p1 = pn.indicators.Number(name='Battery SOC', 
-                          value=power.BatterySOC.resample(time='1h').mean().values[-1], 
-                          format='{value:.0f}%', 
-                          colors=[(25, 'red'), (50, 'gold'), (100, 'green')]
-)
+    #####################################################################
+    # ....Determine today's and yesterday's dates
+    today     = pd.Timestamp.now('UTC')
+    yesterday = pd.Timestamp.now('UTC')-pd.Timedelta('24h')
 
-p2 = pn.indicators.Gauge(name="DC Inv Power", 
-                         value=np.round(power.DCInverterWatts.resample(time='1h').mean().values[-1]), 
-                         bounds=(0, 250), 
-                         format='{value} W', 
-                         colors=[(0.4, 'green'), (0.6, 'gold'), (1, 'red')],
-                         custom_opts={"pointer": {"itemStyle": {"color": 'black'}}}
-)
+    #####################################################################
+    # ....Read data from SLEIGH and MVP
+    power_yesterday = xr.open_dataset('/data/power/level2/power.mvp.level2.1min.'
+                                      + yesterday.strftime('%Y%m%d') + '.000000.nc')
 
-p3 = pn.indicators.Gauge(name="AC Power", 
-                         value=np.round(power.ACOutputWatts.resample(time='1h').mean().values[-1]), 
-                         bounds=(0, 1500), 
-                         format='{value} W',
-                         colors=[(0.167, 'green'), (0.3, 'gold'), (1, 'red')],
-                         custom_opts={"pointer": {"itemStyle": {"color": 'black'}}}
-)
+    power_today = xr.open_dataset('/data/power/level2/power.mvp.level2.1min.'
+                                  + today.strftime('%Y%m%d') + '.000000.nc')
 
-p4 = pn.indicators.Number(name='Battery Voltage', 
-                          value=power.BatteryVoltage.resample(time='1h').mean().values[-1], 
-                          format='{value:.0f} V', 
-                          colors=[(40, 'red'), (45, 'yellow'), (55, 'green'), (60, 'yellow'), (70, 'red')]
-)
+    power = xr.concat([power_yesterday, power_today], dim='time')
 
-row = pn.Row(p1, pn.Spacer(sizing_mode='stretch_width'), 
-             p2, pn.Spacer(sizing_mode='stretch_width'), 
-             p3, pn.Spacer(sizing_mode='stretch_width'), 
-             p4
-)
+    # ....Select the past 24 hours only... 
+    power = power.sel(time=slice(yesterday.to_datetime64(), today.to_datetime64()))
 
-#####################################################################
-# ....Column of time-series plots of battery and power variables
-#overlay = hv.Curve([1, 2, 3], vdims=['A']) * hv.Curve([2, 3, 4], vdims=['A']) * hv.Curve([3, 2, 1], vdims=['B'])
-#overlay.opts(multi_y=True)
+    BatterySOC = xr.DataArray(power.BatterySOC.values,
+                              coords=[("time", power.time.values)], name='SOC [%]')
 
-p5 = BatterySOC.hvplot(grid=True, 
-                       line_width=5, 
-                       height=400, 
-                       title='BATTERIES (most recent measurement from ' + pd.to_datetime(power.time.data[-1]).strftime('%H:%M') +')', 
-                       color='lightseagreen', responsive=True).opts(active_tools=['box_zoom']) * 
-    BatteryWatts.hvplot(grid=True, 
-                        line_width=5, 
-                        height=400, 
-                        color='lightblue', 
-                        responsive=True)
-p5.opts(multi_y=True)
+    BatteryWatts = xr.DataArray(power.BatteryWatts.values,
+                                coords=[("time", power.time.values)], name='BattS [W]')
 
-p6 = SolarWatts_Tot.hvplot(grid=True, 
-                           line_width=5, 
+    SolarWatts_Tot = xr.DataArray(power.SolarWatts_East.values
+                                  + power.SolarWatts_South.values
+                                  + power.SolarWatts_West.values,
+                                  coords=[("time", power.time.values)], name='Total')
+
+    SolarWatts_E = xr.DataArray(power.SolarWatts_East.values,
+                                  coords=[("time", power.time.values)], name='East')
+    SolarWatts_W = xr.DataArray(power.SolarWatts_West.values,
+                                  coords=[("time", power.time.values)], name='West')
+    SolarWatts_S = xr.DataArray(power.SolarWatts_South.values,
+                                  coords=[("time", power.time.values)], name='South')
+
+
+    WindWatts = xr.DataArray(power.WindWatts.values,
+                             coords=[("time", power.time.values)], name='Wind [W]')
+
+    ACOutputWatts = xr.DataArray(power.ACOutputWatts.values,
+                                 coords=[("time", power.time.values)], name='AC [W]')
+
+    DCInverterWatts = xr.DataArray(power.DCInverterWatts.values,
+                                   coords=[("time", power.time.values)], name='DCInverterWatts')
+
+    DCWatts = SolarWatts_Tot + WindWatts - BatteryWatts - DCInverterWatts
+
+    DCWatts.name = 'DC [W]'
+
+    SolarWatts = xr.merge([SolarWatts_Tot, SolarWatts_E, SolarWatts_W, SolarWatts_S])
+
+    # %%
+    p0 = pn.pane.Markdown('# ICECAPS SLEIGH-MVP Dashboard')
+    title = row = pn.Row(p0)
+
+
+    # %%
+    #################### MINIMUM VIABLE POWERSUPPLY #####################
+    #####################################################################
+    # ....Row of battery and power numbers and gauges
+    p1 = pn.indicators.Number(name='Battery SOC', 
+                              value=power.BatterySOC.resample(time='1h').mean().values[-1], 
+                              format='{value:.0f}%',
+                              colors=[(25, 'red'), (50, 'gold'), (100, 'green')]
+    )
+
+    p2 = pn.indicators.Gauge(name='DC Power', 
+                             value=np.round(power.DCWatts.resample(time='1h').mean().values[-1]), 
+                             bounds=(0, 250), 
+                             format='{value} W', 
+                             colors=[(0.4, 'green'), (0.6, 'gold'), (1, 'red')],
+                             tooltip_format='{b} : {c} W',
+    )
+    #                         custom_opts={"pointer": {"itemStyle": {"color": 'black'}}}
+    #)
+
+    p3 = pn.indicators.Gauge(name="AC Power", 
+                             value=np.round(power.ACOutputWatts.resample(time='1h').mean().values[-1]), 
+                             bounds=(0, 1500), 
+                             format='{value} W',
+                             colors=[(0.167, 'green'), (0.3, 'gold'), (1, 'red')],
+                             tooltip_format='{b} : {c} W',
+    )
+    #                         custom_opts={"pointer": {"itemStyle": {"color": 'black'}}}
+    #)
+
+    p4 = pn.indicators.Number(name='Battery Voltage', 
+                              value=power.BatteryVoltage.resample(time='1h').mean().values[-1], 
+                              format='{value:.0f} V', 
+                              colors=[(40, 'red'), (45, 'yellow'), (55, 'green'), (60, 'yellow'), (70, 'red')]
+    )
+
+    row = pn.Row(p1, pn.Spacer(sizing_mode='stretch_width'), 
+                 p2, pn.Spacer(sizing_mode='stretch_width'), 
+                 p3, pn.Spacer(sizing_mode='stretch_width'), 
+                 p4
+    )
+
+    # %%
+    #####################################################################
+    # ....Column of time-series plots of battery and power variables
+    #overlay = hv.Curve([1, 2, 3], vdims=['A']) * hv.Curve([2, 3, 4], vdims=['A']) * hv.Curve([3, 2, 1], vdims=['B'])
+    #overlay.opts(multi_y=True)
+
+    p5 = BatterySOC.hvplot(grid=True, 
                            height=400, 
-                           title='RENEWABLES (most recent measurement from '+pd.to_datetime(power.time.data[-1]).strftime('%H:%M')+')', color='sienna', responsive=True).opts(active_tools=['box_zoom']) * 
-        WindWatts.hvplot(line_width=5, 
-                         height=400, 
-                         color='darkkhaki', 
-                         responsive=True)
-p6.opts(multi_y=True)
+                           title='BATTERIES (most recent measurement from ' + pd.to_datetime(power.time.data[-1]).strftime('%H:%M') +')', 
+                           label='SOC [%]',
+                           color='mediumvioletred', responsive=True).opts(active_tools=['box_zoom']) * \
+        BatteryWatts.hvplot(height=400, 
+                            color='cornflowerblue', 
+                            label='Batts [W]',
+                            responsive=True)
+    p5.opts(multi_y=True)
+
+    splot = SolarWatts.hvplot.scatter(grid=True, 
+                                      s = 2, 
+                                      x = 'time', 
+                                      y = ['Total', 'East', 'West', 'South'],
+                                      color =  ['gold', 'khaki', 'olive', 'darkgoldenrod' ],
+                                  height=400, 
+                                  title='RENEWABLES (most recent measurement from ' + pd.to_datetime(power.time.data[-1]).strftime('%H:%M') +')',
+                               label='Solar [W]',
+                               responsive=True)
+
+    #p6 = splot
+
+    p6 = (splot* \
+          WindWatts.hvplot.scatter(height=400, 
+                                   x = 'time', 
+                                   y = ['Wind [W]'], 
+                                      s = 2, 
+                              color='teal', 
+                              label='Wind',
+                              responsive=True))
+
+    p6.opts(active_tools=['box_zoom'])
+
+    splot = SolarWatts_Tot.hvplot(grid=True, 
+                                  height=400, 
+                                  title='RENEWABLES (most recent measurement from ' + pd.to_datetime(power.time.data[-1]).strftime('%H:%M') +')',
+                               label='Solar [W]',
+                               color='gold', responsive=True)* \
+          SolarWatts_W.hvplot(grid=True, 
+                              label='West',
+                               height=400, 
+                               color='khaki', responsive=True)* \
+          SolarWatts_S.hvplot(grid=True, 
+                              label='South',
+                               height=400, 
+                               color='darkseagreen', responsive=True)* \
+          SolarWatts_E.hvplot(grid=True, 
+                              label='East',
+                               height=400, 
+                               color='tan', responsive=True)
 
 
-p7 = ACOutputWatts.hvplot(grid=True, 
-                          line_width=5, 
-                          height=400, 
-                          title='OUTPUT (most recent measurement from '+pd.to_datetime(power.time.data[-1]).strftime('%H:%M')+')', 
-                          color='firebrick', 
-                          responsive=True).opts(active_tools=['box_zoom']) * 
-            DCWatts.hvplot(line_width=5, 
-                           height=400, 
-                           color='blue', 
-                           responsive=True)
-p7.opts(multi_y=True)
+    # p6 = hv.Overlay([SolarWatts_E.hvplot(title='RENEWABLES (most recent measurement from '+pd.to_datetime(power.time.data[-1]).strftime('%H:%M')+')',
+    #                                      color='tan', label='Solar [W]',
+    #                                      height=400, grid=True, responsive=True), 
+    #                  SolarWatts_S.hvplot(color='darkkhaki', height=400, responsive=True),
+    #                  SolarWatts_W.hvplot(color='darkgoldenrod', height=400, responsive=True),
+    #                  SolarWatts_Tot.hvplot(color='gold',height=400, responsive=True)]) \
+    #                 .opts(
+    #                       responsive=True,
+    #                       active_tools=['box_zoom']) * \
+    # WindWatts.hvplot(height=400, 
+    #                          color='lightblue', 
+    #                          label='Wind [W]',
+    #                          responsive=True).opts(multi_y=True)
+    #p6.opts(multi_y=True)
 
-col = pn.Column(p5, p6, p7)
-#col.servable()
 
-tabs = pn.Tabs(('Minimum Viable Powersupply', pn.Column(row, col)))
+    p7 = ACOutputWatts.hvplot(grid=True, 
+                              height=400, 
+                              title='OUTPUT (most recent measurement from '+pd.to_datetime(power.time.data[-1]).strftime('%H:%M')+')', 
+                              color='firebrick', 
+                              label='AC [W]',
+                              responsive=True).opts(active_tools=['box_zoom']) * \
+                DCWatts.hvplot(height=400, 
+                               color='darkgreen', 
+                               label='DC [W]',
+                               responsive=True)
+    p7.opts(multi_y=True)
 
-#################### INSTRUMENT UPTIME #####################
-tabs.append(('Instrument Uptime', pn.pane.Markdown('# Coming soon...')))
+    # %%
+    col = pn.Column(p5, p6, p7)
+    #col.servable()
 
-#################### NEAR-SURFACE METEOROLOGY #####################
-tabs.append(('Near-surface Meteorology', pn.pane.Markdown('# Coming soon...')))
+    tabs = pn.Tabs(('Minimum Viable Powersupply', pn.Column(title, row, col)))
 
-#################### SURFACE ENERGY BUDGET #####################
-tabs.append(('Surface Energy Budget', pn.pane.Markdown('# Coming soon...')))
+    # %%
+    #################### INSTRUMENT UPTIME #####################
+    tabs.append(('Instrument Uptime', pn.pane.Markdown('# Coming soon...')))
 
-#################### CLOUD PROPERTIES #####################
-tabs.append(('Cloud Properties', pn.pane.Markdown('# Coming soon...')))
+    # %%
+    #################### NEAR-SURFACE METEOROLOGY #####################
+    tabs.append(('Near-surface Meteorology', pn.pane.Markdown('# Coming soon...')))
 
-tabs.servable(title='ICECAPS SLEIGH-MVP Dashboard')
+    # %%
+    #################### SURFACE ENERGY BUDGET #####################
+    tabs.append(('Surface Energy Budget', pn.pane.Markdown('# Coming soon...')))
+
+    # %%
+    #################### CLOUD PROPERTIES #####################
+    tabs.append(('Cloud Properties', pn.pane.Markdown('# Coming soon...')))
+
+    # %%
+    #tabs.servable(title='ICECAPS SLEIGH-MVP Dashboard')
+    #tabs.servable(title='ICECAPS SLEIGH-MVP Dashboard')
+    #tabs.show()
+
+    return tabs
+
+def main():
+
+    while True:           
+
+        try:
+            tabs = process_data()
+            server_thread = pn.serve({'dashboard': tabs }, title='ICECAPS SLEIGH-MVP Dashboard',
+                                     port=6646, websocket_origin="*", show=False, threaded=True)
+
+            for s in range(0,59):
+                print("... we could do work here...")
+                time.sleep(1)
+            server_thread.stop()
+
+            print("Restarting websocket...")
+            
+        except KeyboardInterrupt:   
+            print("Exiting...")
+            exit()
+
+
+# this runs the function main as the main program... functions
+# to come after the main code so it presents in a more logical, C-like, way
+# https://stackoverflow.com/questions/11241523/why-does-python-code-run-faster-in-a-function
+if __name__ == '__main__':
+
+    main()

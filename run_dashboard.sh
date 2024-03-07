7e913ddd2c1d452a0a495af263b4ae6f9e409c20
mg <michael.r.gallagher@noaa.gov>
2024-03-07 04:38:39 +0000

    truly unbuffer things...

--- run_dashboard.sh
+++ run_dashboard.sh
@@ -9,7 +9,7 @@ do
 
     # env var allows all resources to be packed into websocket so
     # apache http proxy can actually locate them 
-    BOKEH_RESOURCES=inline /home/sleigh/sleigh-dashboard/dashboard.py
+    PYTHONUNBUFFERED=1 BOKEH_RESOURCES=inline /home/sleigh/sleigh-dashboard/dashboard.py
 
     sleep 10 # allow time for killing/reloading file etc
 

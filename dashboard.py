2f19c91ebc02568a16a9b449d73ae1eca389b556
mg <michael.r.gallagher@noaa.gov>
2024-03-12 15:49:44 +0000

    cleaner code, merge andrews tabs

--- dashboard.py
+++ dashboard.py
@@ -6,22 +6,27 @@ import hvplot.xarray # noqa
 import holoviews as hv
 import panel as pn
 
-import time, datetime
+import time, datetime, traceback, sys
 
-from tabs import tab_cl61
-
-pn.extension(design='material', template='material')
-
+#pn.extension(design='material', template='material')
+ 
 from multiprocessing import Process
 
 import warnings
 warnings.filterwarnings("ignore")
 
+sys.path.insert(0,'/home/sleigh/sleigh-dashboard/tabs')
+from tab_cl61 import tab_cl61
 
-def process_data():
+alarm_color   = 'palevioletred'
+warning_color = 'khaki'
+happy_color   = 'mediumseagreen'
 
-    pn.extension('echarts', template='fast', nthreads=4, notifications=False)
+pn.extension('echarts', template='fast', nthreads=4, notifications=False)
 
+def power_columns(refresh_button_state=None):
+
+    print(refresh_button_state, type(refresh_button_state))
 
     #####################################################################
     # ....Determine today's and yesterday's dates
@@ -32,94 +37,93 @@ def process_data():
     # ....Read data from SLEIGH and MVP
     power = get_power_data(start_date=yesterday, end_date=today, data_dir='/data/power/level2/')
 
-    tz = datetime.timezone(datetime.timedelta(seconds=0))
+    # get some info about how new the data is on the server
+    tz            = datetime.timezone(datetime.timedelta(seconds=0))
     last_obs_time = pd.to_datetime(power.time.data[-1]).replace(tzinfo=tz)
-    secs_ago =str(int(np.abs((last_obs_time- pd.Timestamp.now('UTC')).total_seconds()/60)))
+    mins_ago      = str(int(np.abs((last_obs_time- pd.Timestamp.now('UTC')).total_seconds()/60)))
+    curr_time     = today.strftime("%H:%M")
+    tcoords       = [('time', power.time.values)]
 
-    BatterySOC = xr.DataArray(power.BatterySOC.values,
-                              coords=[("time", power.time.values)], name='SOC [%]')
-
-    BatteryWatts = xr.DataArray(power.BatteryWatts.values,
-                                coords=[("time", power.time.values)], name='BattS [W]')
+    BatterySOC   = xr.DataArray(power.BatterySOC.values, coords=tcoords, name='SOC [%]')
+    BatteryWatts = xr.DataArray(power.BatteryWatts.values, coords=tcoords, name='BattS [W]')
 
     SolarWatts_Tot = xr.DataArray(power.SolarWatts_East.values
                                   + power.SolarWatts_South.values
-                                  + power.SolarWatts_West.values,
-                                  coords=[("time", power.time.values)], name='Total')
+                                  + power.SolarWatts_West.values, coords=tcoords, name='Total')
 
-    SolarWatts_E = xr.DataArray(power.SolarWatts_East.values,
-                                  coords=[("time", power.time.values)], name='East')
-    SolarWatts_W = xr.DataArray(power.SolarWatts_West.values,
-                                  coords=[("time", power.time.values)], name='West')
-    SolarWatts_S = xr.DataArray(power.SolarWatts_South.values,
-                                  coords=[("time", power.time.values)], name='South')
+    SolarWatts_E = xr.DataArray(power.SolarWatts_East.values  , coords=tcoords, name='East')
+    SolarWatts_W = xr.DataArray(power.SolarWatts_West.values  , coords=tcoords, name='West')
+    SolarWatts_S = xr.DataArray(power.SolarWatts_South.values , coords=tcoords, name='South')
 
-
-    WindWatts = xr.DataArray(power.WindWatts.values,
-                             coords=[("time", power.time.values)], name='Wind [W]')
-
-    ACOutputWatts = xr.DataArray(power.ACOutputWatts.values,
-                                 coords=[("time", power.time.values)], name='AC [W]')
-
-    DCInverterWatts = xr.DataArray(power.DCInverterWatts.values,
-                                   coords=[("time", power.time.values)], name='DCInverterWatts')
+    WindWatts       = xr.DataArray(power.WindWatts.values       , coords=tcoords, name='Wind [W]')
+    ACOutputWatts   = xr.DataArray(power.ACOutputWatts.values   , coords=tcoords, name='AC [W]')
+    DCInverterWatts = xr.DataArray(power.DCInverterWatts.values , coords=tcoords, name='DCInverterWatts')
 
     DCWatts = SolarWatts_Tot + WindWatts - BatteryWatts - DCInverterWatts
-
     DCWatts.name = 'DC [W]'
 
     SolarWatts = xr.merge([SolarWatts_Tot, SolarWatts_E, SolarWatts_W, SolarWatts_S])
 
-    # %%
-    p0 = pn.pane.Markdown('# ICECAPS SLEIGH-MVP Dashboard')
-    title = row = pn.Row(p0)
-
-
-    # %%
     #################### MINIMUM VIABLE POWERSUPPLY #####################
     #####################################################################
-    averagingTime = '10T'    # Currently 10 minutes
-    
-    last_time = today
+    average_time = '10T'    # Currently 10 minutes
+    last_time    = today
 
     # ....Row of battery and power numbers and gauges
-    p1 = pn.indicators.Number(name='Battery SOC', 
-                              value=power.BatterySOC.resample(time=averagingTime).mean().values[-1], 
-                              format='{value:.0f}%',
-                              colors=[(25, 'red'), (50, 'gold'), (100, 'green')]
-    )
+    batt_soc = np.round(power.BatterySOC.resample(time=average_time).mean(skipna=True).values[-1],0) 
+    p1 = pn.indicators.Gauge(name=f'\n\nSOC\n{batt_soc}%', 
+                             value=batt_soc,
+                             format=' ', 
+                             bounds=(0, 100), 
+                             colors=[(0.2, alarm_color), (0.5, warning_color), (1, happy_color)],
+                             title_size=15,
+                             height=220, 
+		             num_splits=5, 
+                             custom_opts={"detail": {"fontSize": "5"}, "axislabel": {"fontSize": "5"}}
+                             )
 
-    p2 = pn.indicators.Gauge(name='DC Power', 
-                             value=np.round(power.DCWatts.resample(time=averagingTime).mean().values[-1]), 
-                             bounds=(0, 250), 
-                             format='{value} W', 
-                             colors=[(0.4, 'green'), (0.6, 'gold'), (1, 'red')],
-                             tooltip_format='{b} : {c} W',
-    )
-    #                         custom_opts={"pointer": {"itemStyle": {"color": 'black'}}}
-    #)
+    batt_v = np.round(power.BatteryVolts.resample(time=average_time).mean(skipna=True).values[-2],1) 
+    p2 = pn.indicators.Gauge(name=f'\n\nBATT\n{batt_v}V', 
+                             value=batt_v,
+                             format=' ', 
+                             bounds=(48, 58), 
+                             colors=[(0.15, alarm_color), (0.3, warning_color), (0.7, happy_color),
+                                     (0.85, warning_color), (1.0, alarm_color)],
+                             title_size=15,
+                             height=220, 
+				             num_splits=5, 
+                             custom_opts={"detail": {"fontSize": "5"}, "axislabel": {"fontSize": "5"}}
+                             )
 
-    p3 = pn.indicators.Gauge(name="AC Power", 
-                             value=np.round(power.ACOutputWatts.resample(time=averagingTime).mean().values[-1]), 
+
+    ac_watts = np.round(power.ACOutputWatts.resample(time=average_time).mean(skipna=True).values[-2],1) 
+    p3 = pn.indicators.Gauge(name=f'\n\nAC\n{batt_v}W', 
+                             value=ac_watts,
+                             format=' ', 
                              bounds=(0, 1500), 
-                             format='{value} W',
-                             colors=[(0.167, 'green'), (0.3, 'gold'), (1, 'red')],
-                             tooltip_format='{b} : {c} W',
-    )
-    #                         custom_opts={"pointer": {"itemStyle": {"color": 'black'}}}
-    #)
+                             colors=[(0.3, happy_color), (0.6, warning_color), (1.0, alarm_color)],
+                             title_size=15,
+                             height=220, 
+				             num_splits=5, 
+                             custom_opts={"detail": {"fontSize": "5"}, "axislabel": {"fontSize": "5"}}
+                             )
 
-    #p4 = pn.indicators.Number(name='Battery Voltage', 
-    #                          value=power.BatteryVoltage.resample(time=averagingTime).mean().values[-1], 
-    #                          format='{value:.1f} V', 
-    #                          colors=[(40, 'red'), (45, 'yellow'), (55, 'green'), (60, 'yellow'), (70, 'red')]
-    #)
+    dc_watts = np.round(power.DCWatts.resample(time=average_time).mean(skipna=True).values[-1],0) 
+    p4 = pn.indicators.Gauge(name=f'\n\nDC\n{batt_soc}W', 
+                             value=dc_watts,
+                             format=' ', 
+                             bounds=(0, 300), 
+                             colors=[(0.4, happy_color), (0.7, warning_color), (1, alarm_color)],
+                             title_size=15,
+                             height=220,
+		             num_splits=5, 
+                             custom_opts={"detail": {"fontSize": "5"}, "axislabel": {"fontSize": "5"}}
+                             )
 
-    row = pn.Row(p1, pn.Spacer(sizing_mode='stretch_width'), 
-                 p2, pn.Spacer(sizing_mode='stretch_width'), 
-                 p3, pn.Spacer(sizing_mode='stretch_width'), 
-#                 p4
-    )
+
+    row = pn.Row(p1, p2, p3, p4, 
+		 pn.Spacer(width=100),
+		 pn.Spacer(sizing_mode='stretch_width'))
 
     # %%
     #####################################################################
@@ -129,7 +133,7 @@ def process_data():
 
     p5 = BatterySOC.hvplot(grid=True, 
                            height=400, 
-                           title='BATTERIES (last data from ' + secs_ago +' mins ago)', 
+                           title='BATTERIES (new data ' + mins_ago +'m ago; '+curr_time+')', 
                            label='SOC [%]',
                            color='mediumvioletred', responsive=True).opts(active_tools=['box_zoom']) * \
         BatteryWatts.hvplot(height=400, 
@@ -142,9 +146,9 @@ def process_data():
                                       s = 2, 
                                       x = 'time', 
                                       y = ['Total', 'East', 'West', 'South'],
-                                      color =  ['gold', 'khaki', 'olive', 'darkgoldenrod' ],
+                                      color =  [warning_color, 'khaki', 'olive', 'darkgoldenrod' ],
                                   height=400, 
-                                  title='RENEWABLES (last data from ' + secs_ago +' mins ago)',
+                                  title='RENEWABLES (new data ' + mins_ago +'m ago; '+curr_time+')',
                                label='Solar [W]',
                                responsive=True)
 
@@ -163,9 +167,9 @@ def process_data():
 
     splot = SolarWatts_Tot.hvplot(grid=True, 
                                   height=400, 
-                                  title='RENEWABLES (last data from ' + secs_ago +' mins ago)',
-                               label='Solar [W]',
-                               color='gold', responsive=True)* \
+                                  title='RENEWABLES (new data ' + mins_ago +'m ago; '+curr_time+')',
+                                  label='Solar [W]',
+                                  color=warning_color, responsive=True)* \
           SolarWatts_W.hvplot(grid=True, 
                               label='West',
                                height=400, 
@@ -179,13 +183,12 @@ def process_data():
                                height=400, 
                                color='tan', responsive=True)
 
-
-    # p6 = hv.Overlay([SolarWatts_E.hvplot(title='RENEWABLES (most recent measurement from '+secs_ago+' mins ago)',
+    # p6 = hv.Overlay([SolarWatts_E.hvplot(title='RENEWABLES (most recent measurement from '+mins_ago+'m ago; '+curr_time+')',
     #                                      color='tan', label='Solar [W]',
     #                                      height=400, grid=True, responsive=True), 
     #                  SolarWatts_S.hvplot(color='darkkhaki', height=400, responsive=True),
     #                  SolarWatts_W.hvplot(color='darkgoldenrod', height=400, responsive=True),
-    #                  SolarWatts_Tot.hvplot(color='gold',height=400, responsive=True)]) \
+    #                  SolarWatts_Tot.hvplot(color=warning_color,height=400, responsive=True)]) \
     #                 .opts(
     #                       responsive=True,
     #                       active_tools=['box_zoom']) * \
@@ -198,7 +201,7 @@ def process_data():
 
     p7 = ACOutputWatts.hvplot(grid=True, 
                               height=400, 
-                              title='OUTPUT (last data from '+secs_ago+' mins ago)', 
+                              title='OUTPUT (new data '+mins_ago+'m ago; '+curr_time+')', 
                               color='firebrick', 
                               label='AC [W]',
                               responsive=True).opts(active_tools=['box_zoom']) * \
@@ -208,11 +211,25 @@ def process_data():
                                responsive=True)
     p7.opts(multi_y=True)
 
-    # %%
     col = pn.Column(p5, p6, p7)
     #col.servable()
 
-    tabs = pn.Tabs(('Minimum Viable Powersupply', pn.Column(title, row, col)))
+    return pn.Column(pn.Column(row), col)
+
+def create_tabs():
+
+    update_button = pn.widgets.Button(name='ICECAPS MELT — MVP Dashboard, click to refresh data',
+                                      button_type='primary', button_style='outline', 
+                                      styles={'width':'96%'}, height=50,
+                                      )
+    pcs = pn.Column(pn.Spacer(height=10), 
+                    pn.Row(pn.Spacer(sizing_mode='stretch_width'), update_button, pn.Spacer(sizing_mode='stretch_width')),
+                    pn.Spacer(height=10), 
+                    pn.panel(pn.bind(power_columns_wrapper, update_button), loading_indicator=True),
+                    styles={'background': 'whitesmoke'})
+
+    #power_title   = pn.pane.Markdown('# ICECAPS MELT — MVP Dashboard' ) 
+    tabs          = pn.Tabs(('Minimum Viable Powersupply', pn.Column(pcs)))
 
     # %%
     #################### INSTRUMENT UPTIME #####################
@@ -230,9 +247,9 @@ def process_data():
     #################### CLOUD PROPERTIES #####################
     tabs.append(('Cloud Properties', pn.pane.Markdown('# Coming soon...')))
 
-    tabs.append(('CL61', tab_cl61.tab_cl61()))
-
     # %%
+    tabs.append(('CL61', tab_cl61()))
+
     #tabs.servable(title='ICECAPS SLEIGH-MVP Dashboard')
     #tabs.servable(title='ICECAPS SLEIGH-MVP Dashboard')
     #tabs.show()
@@ -244,13 +261,15 @@ def get_power_data(start_date, end_date, data_dir='/data/power/level2/'):
     import glob
 
     data_list = []
+
+    ds_list = []
     for curr_date in pd.date_range(start_date, end_date+datetime.timedelta(1)):
         data_list+=glob.glob(f'{data_dir}/power.mvp.level2.1min.'+curr_date.strftime('%Y%m%d')+ '.*.nc')
-
-
-    power = xr.open_mfdataset(data_list)
-
-    #power = xr.concat([power_yesterday, power_today], dim='time')
+        ds = xr.open_dataset(glob.glob(f'{data_dir}/power.mvp.level2.1min.'+curr_date.strftime('%Y%m%d')+ '.*.nc')[0])
+        ds_list.append(ds)
+#    power = xr.open_mfdataset(data_list, drop_variables=['num_batts_installed, BattsOnline'])
+    
+    power = xr.concat(ds_list, dim='time')
 
     # ... select the requested range only... 
     power = power.sel(time=slice(start_date.to_datetime64(), end_date.to_datetime64()))
@@ -260,32 +279,55 @@ def get_power_data(start_date, end_date, data_dir='/data/power/level2/'):
 
 def launch_server_process(panel_dict):
 
-    server_thread = pn.serve(panel_dict, title='ICECAPS SLEIGH-MVP Dashboard')
-                            # port=5006, websocket_origin="*", show=False)
+    server_thread = pn.serve(panel_dict, title='ICECAPS SLEIGH-MVP Dashboard',
+                             port=6646, websocket_origin="*", show=False)
 
     return True # not necessary but explicit
 
-def main():
+def power_columns_wrapper(button_status):
     try:
-        tabs = process_data()
-        panel_dict = {'dashboard': tabs} # if you make other pages, add them here... 
+        return power_columns(button_status)
+    except:
+        print_traceback(traceback.format_exc(), f"FAILURE")
 
-        p = Process(target=launch_server_process, args=(panel_dict,))
-        p.start()
-        print('process started')
-        
-    except KeyboardInterrupt:   
-        print("Exiting...")
-        exit()
+def print_traceback(thetb, error_msg):
 
-    except Exception as e:
-        print(e)
+    print(f"!!! ———————————————————————————————————————————————————————— !!! " , file=sys.stderr)
+    print(f"!!! {error_msg} "                                                  , file=sys.stderr)
+    print("\n\n"                                                               , file=sys.stderr)
+    print(thetb                                                                   , file=sys.stderr)
+    print(f"!!! ———————————————————————————————————————————————————————— !!! " , file=sys.stderr)
 
+def main():
+
+    while True:           
+
+        try:
+            tabs = create_tabs()
+            
+            panel_dict = {'dashboard': tabs} # if you make other pages, add them here... 
+
+            p = Process(target=launch_server_process, args=(panel_dict,))
+            p.start()
+            for s in range(0,6000):
+                if s % 600 ==0 : print("... we could do work here... but we're still alive")
+                time.sleep(1)
+
+            print("Restarting websocket...")
+
+            p.join(timeout=1); p.terminate()
+            
+        except KeyboardInterrupt:   
+            print("Exiting...")
+            exit()
+
+        except Exception as e:
+            print(e)
 
 
 # this runs the function main as the main program... functions
 # to come after the main code so it presents in a more logical, C-like, way
 # https://stackoverflow.com/questions/11241523/why-does-python-code-run-faster-in-a-function
-#if __name__ == '__main__':
+if __name__ == '__main__':
 
-main()
+    main()

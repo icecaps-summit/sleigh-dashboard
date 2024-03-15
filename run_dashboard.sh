575ee97d6b76aba2b32f460d5ad687f731ce2471
Andrew Martin <eeasm@leeds.ac.uk>
2024-03-15 19:01:56 +0000

    actual correction to run_dashboard.py

--- run_dashboard.sh
+++ run_dashboard.sh
@@ -10,7 +10,7 @@ do
 
     # env var allows all resources to be packed into websocket so
     # apache http proxy can actually locate them 
-    PYTHONUNBUFFERED=1 BOKEH_RESOURCES=inline /home/sleigh/sleigh-dashboard/dashboard.py
+    PYTHONUNBUFFERED=1 BOKEH_RESOURCES=inline /home/website_admin/sleigh-dashboard/dashboard.py
 
     sleep 10 # allow time for killing/reloading file etc
 

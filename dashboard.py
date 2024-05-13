fce69b8c3fee6a6a6e0d6fc6f833708be72c15de
Andrew Martin <eeasm@leeds.ac.uk>
2024-05-13 18:35:52 +0000

    instrument dashboard ready for first deployment

---
+++ dashboard.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env -S python3 -u
+import numpy as np
+import pandas as pd
+import xarray as xr
+import hvplot.xarray # noqa
+import holoviews as hv
+import panel as pn
+import datetime as dt
+
+import time, traceback, sys
+
+import dashboard_instrument, dashboard_thematic
+
+pn.extension(design='material', template='material')
+
+#pn.extension(design='material', template='material')
+ 
+from multiprocessing import Process
+
+import warnings
+warnings.filterwarnings("ignore")
+
+pn.extension('echarts', template='fast', nthreads=4, notifications=False)
+
+# Create the standard global datetime picker choices. Specific datetime-limitted dashboards (i.e. melt events) should have their datetime ranges specified in their files
+start = dt.datetime(2024,5,9)
+end = None#dt.datetime(2024,3,25) # deliberately including days at the end where data doesn't exist, DataLoader should be impervious to these problems...
+now = dt.datetime.now()
+dtr_end = dt.datetime(year=now.year, month=now.month, day=now.day)
+one_day = dt.timedelta(days=1)
+dtr = (dtr_end-one_day, dtr_end) # should start by displaying two days of data
+dtp_args = {'value':dtr, 'start':start, 'end':end, 'name':'global dtp picker'}
+
+db_instrument = lambda: dashboard_instrument.dashboard_instruments(dtp_args, dashboard_instrument.create_dld())
+db_thematic = lambda: dashboard_thematic.dashboard_thematic(dtp_args, dashboard_instrument.create_dld())
+
+
+
+def launch_server_process(panel_dict, port):
+    server_thread = pn.serve(panel_dict, title='ICECAPS SLEIGH-MVP Dashboard',
+                             port=port, websocket_origin='*', show=False)
+    return True # not necessary but explicit
+
+
+def print_traceback(thetb, error_msg):
+
+    print(f"!!! ———————————————————————————————————————————————————————— !!! " , file=sys.stderr)
+    print(f"!!! {error_msg} "                                                  , file=sys.stderr)
+    print("\n\n"                                                               , file=sys.stderr)
+    print(thetb                                                                   , file=sys.stderr)
+    print(f"!!! ———————————————————————————————————————————————————————— !!! " , file=sys.stderr)
+
+
+def main(port=6646):
+    while True:           
+        try:
+            panel_dict = {
+                'instrument': db_instrument,
+                #'thematic': db_thematic
+            } # if you make other pages, add them here... 
+
+            p = Process(target=launch_server_process, args=(panel_dict,port))
+            p.start()
+            for s in range(0,6000):
+                if s % 600 ==0 : print("... we could do work here... but we're still alive")
+                time.sleep(1)
+
+            print("Restarting websocket...")
+
+            p.join(timeout=1); p.terminate()
+            
+        except KeyboardInterrupt:   
+            print("Exiting...")
+            exit()
+
+        except Exception as e:
+            print(e)
+
+        finally:
+            p.terminate()
+
+
+# this runs the function main as the main program... functions
+# to come after the main code so it presents in a more logical, C-like, way
+# https://stackoverflow.com/questions/11241523/why-does-python-code-run-faster-in-a-function
+if __name__ == '__main__':
+    PORT = 6646
+
+    import argparse
+    parser = argparse.ArgumentParser(description='Run the dashboard to display the summarised data from the ICECAPS MELT Raven 2024 deployment.')
+    parser.add_argument('-pd', action='store_true', help="Include this flag to chnage the port from 6646 (deployment) to 5006 (pre-deployment).")
+    #parser.add_argument('')
+    args = parser.parse_args()
+    pd = args.pd
+    if pd:
+        PORT = 5006
+    
+    main(port=PORT)
--- dashboard_thematic.py
+++ dashboard_thematic.py
@@ -1,7 +1,7 @@
 '''Author: Andrew Martin
-Creation Date: 16/4/24
+Creation Date: 13/5/24
 
-Script to implement the instrument dashboard for ICECAPS MELT.
+Script to implement the thematic dashboard for ICECAPS MELT.
 '''
 import warnings
 warnings.filterwarnings('once')
@@ -16,52 +16,28 @@ import numpy as np
 import traceback
 
 import sleigh_dashboard as dashboard
+import tabs.thematic
 import tabs.instrument
 
-# DataLoader objects need to be defined to control the dataflow in the program
 DL_asfs = tabs.instrument.tab_asfs.DL_asfs_slow
-DL_cl61 = tabs.instrument.tab_cl61.DL_cl61
-DL_gfs = tabs.instrument.tab_gfs.DL_gfs
-DL_gpr5 = tabs.instrument.tab_gpr.DL_gpr5
-DL_gpr7 = tabs.instrument.tab_gpr.DL_gpr7
-DL_mrr = tabs.instrument.tab_mrr.DL_mrr
-DL_mwr = tabs.instrument.tab_mwr.DL_mwr
-DL_mvp = tabs.instrument.tab_mvp.DL_mvp
-DL_simba = tabs.instrument.tab_simba.DL_simba
 
-def create_dld():
-    dld = {
-        'asfs': DL_asfs,
-        'cl61': DL_cl61,
-        'gfs': DL_gfs,
-        'gpr5':DL_gpr5,
-        'gpr7':DL_gpr7, 
-        'mrr': DL_mrr,
-        'mwr': DL_mwr,
-        'mvp':DL_mvp,
-        'simba':DL_simba,
-    }
-    return dld
+dld = {
+    'asfs':DL_asfs
+}
 
 def get_tabview(dld, augment) -> dashboard.TabView.TabView:
     tabview = dashboard.TabView.TabView(
         tablist=[
             #get_mvp_tab(augment),
-            tabs.instrument.tab_cl61.get_lidar_tab(augment),
-            tabs.instrument.tab_mrr.get_radar_tab(augment),
-            tabs.instrument.tab_mwr.get_mwr_tab(augment),
-            tabs.instrument.tab_asfs.get_asfs_tab(augment),
-            tabs.instrument.tab_gpr.get_gpr_tab(augment),
-            tabs.instrument.tab_simba.get_simba_tab(augment),
-            tabs.instrument.tab_mvp.get_mvp_tab(augment),
-            tabs.instrument.tab_gfs.get_gfs_tab(augment),
+            tabs.thematic.tab_met.get_met_tab(augment)
         ],
         dld=dld,
         augment_dims=augment
     )
     return tabview
 
-def dashboard_instruments(dtp_args,dld):
+
+def dashboard_thematic(dtp_args,dld):
     db = dashboard.Dashboard.Dashboard(
         dtp_args=dtp_args,
         dict_DL=dld,
@@ -81,5 +57,6 @@ if __name__ == '__main__':
     dtr = (dtr_end-one_day, dtr_end) # should start by displaying two days of data
     dtp_args = {'value':dtr, 'start':start, 'end':end, 'name':'global dtp picker'}
 
-    serve_dashboard = lambda: dashboard_instruments(dtp_args, create_dld())
+    serve_dashboard = lambda: dashboard_thematic(dtp_args, dld)
     pn.serve(serve_dashboard,title='OOP Dashboard', port=5006, websocket_origin='*', show=True )
+
